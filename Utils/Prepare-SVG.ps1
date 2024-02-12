a3147c6e4eeee9465508dce3746ff88d772b9fb1
Denis Molodtsov <denis-ne@yandex.ru>
2024-02-11 22:41:29 -0500

    fixed a but with backgrounds

--- Utils/Prepare-SVG.ps1
+++ Utils/Prepare-SVG.ps1
@@ -7,9 +7,10 @@ foreach ($file in Get-ChildItem -Path svg\ -Filter *.svg) {
     $content = $content -replace 'fill="#E5432F"', 'class="secondary"'
     $content = $content -replace 'fill="#F6F6F6"', 'class="frame"'
     $content = $content -replace 'fill="#EEEDF5"', 'class="background"'
+    # save changes
+    Set-Content -Path $file.FullName -Value $content
+
 }
-# save changes
-Set-Content -Path $file.FullName -Value $content
 
 # update the images.txt file with the list of svg files
 Get-ChildItem -Path svg\ -Filter *.svg | ForEach-Object { $_.Name } | Set-Content -Path images.txt
